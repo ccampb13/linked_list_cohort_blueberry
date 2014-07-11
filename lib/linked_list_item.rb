adbdccb6b8b35ad20b1fb098743f8134c8d2c2e1
Chad Campbell <ccampb13@gmail.com>
2014-07-11 10:25:02 -0500

    Implment LinkedListItem

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,27 @@
 class LinkedListItem
   include Comparable
+
+  attr_reader :next_item
+  attr_reader :payload
+
+  def initialize(value)
+    @payload = value
+  end
+
+  def next_item=(other_item)
+    raise ArgumentError if other_item === self
+    @next_item = other_item
+  end
+
+  def last?
+    @next_item.nil?
+  end
+
+  def <=>(other_item)
+    self.payload.to_s <=> other_item.payload.to_s
+  end
+
+  def ===(other_item)
+    self.equal? other_item
+  end
 end
